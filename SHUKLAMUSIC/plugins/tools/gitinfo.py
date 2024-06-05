c47a3e3e4a0301d7d44a50b3e2ee12bc62c44030
⚠️ ✘ 𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏 ✘ ⚠️ 🚩 <154514127+wwwlbs22@users.noreply.github.com>
2024-06-06 04:49:33 +0530

    Update gitinfo.py

--- SHUKLAMUSIC/plugins/tools/gitinfo.py
+++ SHUKLAMUSIC/plugins/tools/gitinfo.py
@@ -17,7 +17,7 @@ async def daxxhub(_, message):
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git itzshukla")
+        await message.reply_text("/git wwwlbs22)
         return
 
     username = message.text.split(None, 1)[1]
